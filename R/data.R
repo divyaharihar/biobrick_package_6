361ce94f1aef9d0f6e4d3ebe5840cd19d3ece410
VessorMessor <viktor.grunddal@gmail.com>
2023-04-15 12:37:19 +0000

    Updates

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Title
+# Codon table
+#' Description
+# A variable describing codons and their associated residues
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
